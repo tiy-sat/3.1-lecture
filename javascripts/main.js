3e6876125f9010d444e78bdfd2371167fcbde1da
jasonvanderslice <jlvanderslice@gmail.com>
2016-08-15 11:04:43 -0500

    Created dynamic usage of input value and xhr send

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function(){
   var request = new XMLHttpRequest();
 
   var moviesList = document.querySelector(".movies");
+  var titleElement = document.querySelector(".search__title");
+  var submitElement = document.querySelector(".search__submit");
 
   request.addEventListener('load', function(e){
     var responseJSON  = JSON.parse(e.target.response);
@@ -20,12 +22,19 @@ document.addEventListener("DOMContentLoaded", function(){
       // Closing the listen item tag
       listHTML += "</li>";
     });
+    // Reset inner html to just be current results
     moviesList.innerHTML = listHTML;
   });
 
-  // Open the request
-  // Takes two arguments: type of request, and url to send later
-  request.open("GET", "http://www.omdbapi.com/?s=harry potter&plot=full");
-  // Sending the request (to get info back)
-  request.send(null);
+  submitElement.addEventListener("click", function(e){
+    // stops the form from submitting by default
+    e.preventDefault();
+    // Grab text from search title input field
+    var titleValue = titleElement.value;
+    // Open the request WITH*** the title concat'd
+    // Takes two arguments: type of request, and url to send later
+    request.open("GET", "http://www.omdbapi.com/?s=" + titleValue);
+    // Sending the request (to get info back)
+    request.send(null);
+  });
 });
